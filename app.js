be246f28061f1defcf024a9a93f73044e43e231b
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-06-01 23:22:10 +0530

    A lot

--- app.js
+++ app.js
@@ -18,9 +18,42 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index", {title:"Chess Game"});
+  res.render("index", { title: "Chess Game" });
 });
 
+io.on("connection", function (uniquesocket) {
+  console.log("connected");
+
+  if (!player.white) {
+    player.white = uniquesocket.id;
+    uniquesocket.emit("playerRole", "w");
+  } else if (!player.black) {
+    player.black = uniquesocket.id;
+    uniquesocket.emit("playerRole ", "b");
+  } else {
+    uniquesocket.emit("spectatorRole");
+  }
+
+  uniquesocket.on("disconnect ", function () {
+    if (uniquesocket.id === player.white) {
+      delete player.white;
+    } else if (uniquesocket.id === player.black) {
+      delete player.black;
+    }
+  });
+
+  uniquesocket.on("move", (move) => {
+    try {
+      if (chess.turn() === "w" && uniquesocket.id !== player.white) return;
+      if (chess.turn() === "b" && uniquesocket.id !== player.black) return;
+
+      const result = chess.move(move);
+      if (result) {
+        currentPlayer = chess.turn();
+      }
+    } catch (error) {}
+  });
+});
 server.listen(3000, function () {
   console.log("listening on port 3000");
 });
